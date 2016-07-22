ac3e2500df57d42db815de754419a8e0136c17f7
Yuri_Karpov <Yuri_Karpov@EPRURYAW0064.moscow.epam.com>
2016-07-22 10:14:22 +0300

    screenshot path change

--- SeleniumReporting/src/test/java/com/epam/yuri_karpov/selenium/GmailTest.java
+++ SeleniumReporting/src/test/java/com/epam/yuri_karpov/selenium/GmailTest.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
